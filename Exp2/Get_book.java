2d0a6915e0679681c05c84ca0eeeb5e40e41a0e7
Lavanya-2004 <113500289+Lavanya-2004@users.noreply.github.com>
2023-08-16 14:54:41 +0530

    Add files via upload

---
+++ Exp2/Get_book.java
@@ -0,0 +1,96 @@
+
+
+/**
+ * Class Get_book
+ */
+public class Get_book {
+
+  //
+  // Fields
+  //
+
+  private void Book_ID;
+  private void Member_ID;
+  private void Duration;
+  
+  //
+  // Constructors
+  //
+  public Get_book () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of Book_ID
+   * @param newVar the new value of Book_ID
+   */
+  private void setBook_ID (void newVar) {
+    Book_ID = newVar;
+  }
+
+  /**
+   * Get the value of Book_ID
+   * @return the value of Book_ID
+   */
+  private void getBook_ID () {
+    return Book_ID;
+  }
+
+  /**
+   * Set the value of Member_ID
+   * @param newVar the new value of Member_ID
+   */
+  private void setMember_ID (void newVar) {
+    Member_ID = newVar;
+  }
+
+  /**
+   * Get the value of Member_ID
+   * @return the value of Member_ID
+   */
+  private void getMember_ID () {
+    return Member_ID;
+  }
+
+  /**
+   * Set the value of Duration
+   * @param newVar the new value of Duration
+   */
+  private void setDuration (void newVar) {
+    Duration = newVar;
+  }
+
+  /**
+   * Get the value of Duration
+   * @return the value of Duration
+   */
+  private void getDuration () {
+    return Duration;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void get()
+  {
+  }
+
+
+  /**
+   */
+  public void check_status()
+  {
+  }
+
+
+}
