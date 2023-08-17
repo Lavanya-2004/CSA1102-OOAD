d5951ad4cbf6bf83da735aaef0a7de4f539ca098
Lavanya-2004 <113500289+Lavanya-2004@users.noreply.github.com>
2023-08-17 14:24:01 +0530

    Add files via upload

---
+++ exp12 Foreign trading system/stirng.java
@@ -0,0 +1,31 @@
+
+
+/**
+ * Class stirng
+ */
+public class stirng {
+
+  //
+  // Fields
+  //
+
+  
+  //
+  // Constructors
+  //
+  public stirng () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  //
+  // Other methods
+  //
+
+}
