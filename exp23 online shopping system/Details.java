629c69d248b4f0858fd2a999e2c44fe91cdfb2e4
Lavanya-2004 <113500289+Lavanya-2004@users.noreply.github.com>
2023-08-22 09:04:43 +0530

    Add files via upload

---
+++ exp23 online shopping system/Details.java
@@ -0,0 +1,96 @@
+
+
+/**
+ * Class Details
+ */
+public class Details {
+
+  //
+  // Fields
+  //
+
+  private void name;
+  private void address;
+  private void email;
+  
+  //
+  // Constructors
+  //
+  public Details () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of name
+   * @param newVar the new value of name
+   */
+  private void setName (void newVar) {
+    name = newVar;
+  }
+
+  /**
+   * Get the value of name
+   * @return the value of name
+   */
+  private void getName () {
+    return name;
+  }
+
+  /**
+   * Set the value of address
+   * @param newVar the new value of address
+   */
+  private void setAddress (void newVar) {
+    address = newVar;
+  }
+
+  /**
+   * Get the value of address
+   * @return the value of address
+   */
+  private void getAddress () {
+    return address;
+  }
+
+  /**
+   * Set the value of email
+   * @param newVar the new value of email
+   */
+  private void setEmail (void newVar) {
+    email = newVar;
+  }
+
+  /**
+   * Get the value of email
+   * @return the value of email
+   */
+  private void getEmail () {
+    return email;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void update()
+  {
+  }
+
+
+  /**
+   */
+  public void select()
+  {
+  }
+
+
+}
